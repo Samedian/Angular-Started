dd7d6d2ef3b60aa83064aa53f4e294802430ed81
Samedian <goel26samarth@gmail.com>
2023-01-11 19:01:57 +0530

    Component

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: 'app-servers',
+  // selector: '[app-servers]', //<div app-servers></div>
+  selector: '.app-servers', //<div class='app-servers'></div>
+  // templateUrl: './servers.component.html',
+  template:'<app-server></app-server><hr><app-server></app-server>',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
